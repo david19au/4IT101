8fd5bcaffc816a516360317af57cae0acfa0fb73
david19au <david19au@gmail.com>
2022-03-15 17:13:16 +0100

    Nový main + začátek nového setu

---
+++ 4cviko/src/cz/vse/druhyprojekt/Main2.java
@@ -0,0 +1,10 @@
+package cz.vse.druhyprojekt;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Set<Human> set = new HashSet<>();
+    }
+}
