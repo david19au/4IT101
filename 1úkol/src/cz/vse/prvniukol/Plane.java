9c2780ebe052345f4e767f764d19b4c5465a43b5
david19au <david19au@gmail.com>
2022-03-17 22:55:04 +0100

    Třída Plane - nastavení fieldů, getterů, setter a konstruktoru

--- 1úkol/src/cz/vse/prvniukol/Plane.java
+++ 1úkol/src/cz/vse/prvniukol/Plane.java
@@ -1,4 +1,28 @@
 package cz.vse.prvniukol;
 
 public class Plane {
+    private int identificationNumber;
+    private String manufacturer;
+    private Pilot pilot;
+
+    public int getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public Pilot getPilot() {
+        return pilot;
+    }
+
+    public void setPilot(Pilot pilot) {
+        this.pilot = pilot;
+    }
+
+    public Plane(int identificationNumber, String manufacturer) {
+        this.identificationNumber = identificationNumber;
+        this.manufacturer = manufacturer;
+    }
 }
