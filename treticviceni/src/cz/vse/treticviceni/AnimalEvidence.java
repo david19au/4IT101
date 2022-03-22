b003020ed88023d6ceb825501ba83bccc231262d
david19au <david19au@gmail.com>
2022-03-22 16:37:00 +0100

    Boolean pro kontrolu odebrání

---
+++ treticviceni/src/cz/vse/treticviceni/AnimalEvidence.java
@@ -0,0 +1,13 @@
+package cz.vse.treticviceni;
+
+public class AnimalEvidence {
+
+    public boolean addAnimal(Animal animal) {
+
+    }
+
+    public boolean removeAnimal(Animal animal) {
+
+    }
+
+}
