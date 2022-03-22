608db48d25adb1b1cd7659b80b508e69e6245e03
david19au <david19au@gmail.com>
2022-03-22 17:49:41 +0100

    Testy

--- treticviceni/src/cz/vse/treticviceni/AnimalEvidence.java
+++ treticviceni/src/cz/vse/treticviceni/AnimalEvidence.java
@@ -8,16 +8,26 @@ public class AnimalEvidence {
 
     private Set<Animal> animals = new HashSet<>();
 
+    // add + remove vrací boolean, pokud se podaří tak true, pokud ne, tak false
     public boolean addAnimal(Animal animal) {
+        return animals.add(animal);
 
     }
 
     public boolean removeAnimal(Animal animal) {
-
+        return animals.remove(animal);
     }
 
-    public Collection<Animal> animalsOwned(Owner owner) { //Kolekce se použije pokud je mi jedno jestli je to Set nebo List
 
+    public Collection<Animal> animalsOwned(Owner owner) { //Kolekce se použije pokud je mi jedno jestli je to Set nebo List
+        Set<Animal> toReturn = new HashSet<>(); //vytvoření "proměnné" jménem toReturn
+        for (Animal animal : animals) { //pro každý animal v setu animals
+            if (animal.getOwner().equals(owner)) { // tečka odděluje levou a pravou stranu
+                toReturn.add(animal); //pokud se najde nějaké animal které má toho porovnávaného majitele, tak se přidá do proměnné
+            }
+
+        }
+        return toReturn; //vrácení té kolekce
     }
 
 }
---
+++ treticviceni/tests/cz/vse/treticviceni/AnimalEvidenceTest.java
@@ -0,0 +1,61 @@
+package cz.vse.treticviceni;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*; //zkrátí se to jméno viz. dole - je tam jenom assertEquals, ne assertions.assertEquals
+
+public class AnimalEvidenceTest {
+
+    @Test
+    public void testAddAnimal() {
+        AnimalEvidence animalEvidence = new AnimalEvidence();
+        Owner owner = new Owner("Karel", 18);
+        Animal animal = new Animal("hamster", "Franta", owner);
+        Animal animal2 = new Animal("dog", "Hafík", owner);
+
+
+        //assertEquals se dá zjednodušit na assertTrue a assertFalse
+        assertTrue(animalEvidence.addAnimal(animal)); //porovnání hodnot
+        assertFalse(animalEvidence.addAnimal(animal)); //kontrola že se neuložily duplicity
+
+        assertEquals(false, animalEvidence.addAnimal(animal2)); //kontrola že se neuložily duplicity
+
+    }
+
+    @Test
+    public void testRemoveAnimal() {
+        AnimalEvidence animalEvidence = new AnimalEvidence();
+        Owner owner = new Owner("Karel", 18);
+        Animal animal = new Animal("hamster", "Franta", owner);
+        Animal animal2 = new Animal("dog", "Hafík", owner);
+
+        assertFalse(animalEvidence.removeAnimal(animal));
+        animalEvidence.addAnimal(animal);
+        assertTrue(animalEvidence.removeAnimal(animal));
+        assertFalse(animalEvidence.removeAnimal(animal));
+    }
+
+    @Test
+    public void testanimalsOwnded() {
+        AnimalEvidence animalEvidence = new AnimalEvidence();
+        Owner owner1 = new Owner("Karel", 18);
+        Owner owner2 = new Owner("Jana", 19);
+        Animal animal = new Animal("hamster", "Franta", owner1);
+        Animal animal2 = new Animal("dog", "Hafík", owner1);
+        Animal animal3 = new Animal("cat", "Mňau", owner2);
+
+        assertEquals(Set.of(), animalEvidence.animalsOwned(owner1));
+
+        animalEvidence.addAnimal(animal);
+        animalEvidence.addAnimal(animal2);
+        animalEvidence.addAnimal(animal3);
+
+        assertEquals(Set.of(animal, animal2), animalEvidence.animalsOwned(owner1));
+        assertEquals(Set.of(animal3), animalEvidence.animalsOwned(owner2));
+
+    }
+
+}
