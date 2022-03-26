7324419c7b291343d60ca7b72e0b249b089ca453
david19au <david19au@gmail.com>
2022-03-26 15:30:05 +0100

    misc v mainu

--- 1úkol/src/cz/vse/prvniukol/Main.java
+++ 1úkol/src/cz/vse/prvniukol/Main.java
@@ -26,7 +26,10 @@ public class Main {
         System.out.println(plane1);
 
         cz.vse.prvniukol.PlaneEvidence planeEvidence = new PlaneEvidence();
+        planeEvidence.addPlane(plane0);
         planeEvidence.addPlane(plane1);
         System.out.println(planeEvidence.getUnoccupiedPlanes().size());
+        System.out.println(planeEvidence.planeList);
+
     }
 }
