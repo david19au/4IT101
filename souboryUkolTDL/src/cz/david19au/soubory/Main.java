872b638033940bd1121438ee065320e96797c603
david19au <david19au@gmail.com>
2022-04-27 11:47:45 +0200

    úkol ntb

--- souboryUkolTDL/src/cz/david19au/soubory/Main.java
+++ souboryUkolTDL/src/cz/david19au/soubory/Main.java
@@ -4,14 +4,17 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        ZpracovaniSouboru zpracovaniSouboru = new ZpracovaniSouboru();
+        File soubor = new File(args[0]);
 
-        if(args.length == 0) {
-            System.out.println("V parametrech chybí soubor.");
-        } else {
-            File file = new File(args[0]);
-            zpracovaniSouboru.zpracovani(file);
-        }
+        if(args.length != 1);
+            System.out.println("Bylo zadáno více argumentů, zadejte prosím POUZE 1 cestu k 1 souboru.");
+
+            if(!soubor.exists())
+                System.out.println("Zadaná cesta je špatná, nebo soubor neexistuje.");
+            if(soubor.isDirectory())
+                System.out.println("Toto je složka, ne soubor.");
+
+            ZpracovaniSouboru.zpracovani(soubor);
     }
-
 }
+
